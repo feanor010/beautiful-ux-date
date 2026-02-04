d4573350037a5ec7741b1b3668e1b66c2599bb82
artem <temathebst@gmail.com>
2026-02-04 21:18:01 +0600

    remove unused components

--- src/components/examples/index.ts
+++ src/components/examples/index.ts
@@ -1,7 +1,4 @@
 // Export all date input examples
 export { ButtonGrid } from './ButtonGrid';
-export { ShiftingMask } from './ShiftingMask';
-export { RestrictedCalendar } from './RestrictedCalendar';
-export { RandomOrderSelects } from './RandomOrderSelects';
 export { ProgressiveSelects } from './ProgressiveSelects';
 export { PacmanDigits } from './PacmanDigits';
