6032bdf8097a8498be327f3a7a4ae6124d37c3f4
artem <temathebst@gmail.com>
2026-02-06 01:02:12 +0600

    add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const basePath = repoName ? `/${repoName}/` : '/'
+
 export default defineConfig({
+  base: basePath,
   plugins: [react()],
 })
